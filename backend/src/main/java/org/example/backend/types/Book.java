a45409df034974fd6810e5ae8f5311870037778e
lynxwiltner <lynx@wiltner.net>
2025-05-12 16:07:50 +0200

    add feature to update Book

--- backend/src/main/java/org/example/backend/types/Book.java
+++ backend/src/main/java/org/example/backend/types/Book.java
@@ -1,6 +1,9 @@
 package org.example.backend.types;
 
+import lombok.With;
 import org.springframework.data.annotation.Id;
 
-public record Book (String title, String author, @Id int isbn, String summary, String image, int totalAmount, int totalBookedAmount) {
+@With
+public record Book( String title, String author, @Id int isbn, String summary, String image, int totalAmount,
+                    int totalBookedAmount ) {
 }
