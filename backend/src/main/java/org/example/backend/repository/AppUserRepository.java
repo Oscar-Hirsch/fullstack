23b25d6fd551baf916a870989a76e2422ac32c77
Oscar-Hirsch <oscar@completeaudio.com>
2025-05-21 14:07:16 +0200

    Revert "domtheing"
    This reverts commit 6001d2086069045b7a01d3e8ede3b2bace95ce60.

--- backend/src/main/java/org/example/backend/repository/AppUserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend.repository;
-
-import org.example.backend.types.AppUser;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AppUserRepository extends MongoRepository<AppUser, String> {
-}
--- backend/src/main/java/org/example/backend/types/AppUser.java
+++
@@ -1,6 +0,0 @@
-package org.example.backend.types;
-
-import lombok.Builder;
-
-@Builder
-public record AppUser (String id, String username, String role){}
