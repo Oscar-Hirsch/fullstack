b6cc38095eae02ae85bd46b8eab24bbf61dff260
Oscar-Hirsch <oscar@completeaudio.com>
2025-05-21 15:28:43 +0200

    add feature to fetch books by isbn when creating a book

--- frontend/src/components/ButtonComponent.tsx
+++ frontend/src/components/ButtonComponent.tsx
@@ -3,8 +3,9 @@ export default function ButtonComponent(props: {
   onClick?: () => void;
   label: string;
   disabled?: boolean;
+  type?: "submit" | "reset" | "button" | undefined;
 }) {
-  const { onClick, label } = props;
+  const { onClick, label, type = "submit" } = props;
 
   const className =
     "transition-colors p-2 border rounded-[4px] hover:bg-blue-100 active:bg-blue-300 disabled:bg-gray-400 disabled:border-transparent disabled:text-gray-700 disabled:shadow-none shadow" +
@@ -12,7 +13,12 @@ export default function ButtonComponent(props: {
     props.className;
 
   return (
-    <button className={className} disabled={props.disabled} onClick={onClick}>
+    <button
+      className={className}
+      disabled={props.disabled}
+      onClick={onClick}
+      type={type}
+    >
       {label}
     </button>
   );
